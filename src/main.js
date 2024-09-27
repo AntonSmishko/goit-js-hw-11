955d549757b559caae795e90bb9c7d02f4d00fc7
RooK| <anton.smishko2014@gmail.com>
2024-09-27 16:29:37 +0300

    update

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
+const linkEl = {
+  form: document.querySelector('.form'),
+};
 
+linkEl.form.addEventListener('submit', formHandler);
+
+function formHandler(e) {
+  e.preventDefault();
+  const query = e.target.elements['search-area'].value.trim();
+  console.log(query);
+}
