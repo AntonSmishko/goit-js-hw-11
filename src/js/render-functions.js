df075e3fd70d52503d6fb12b113c714a12e23d44
RooK| <anton.smishko2014@gmail.com>
2024-09-27 17:36:54 +0300

    add render
    render and styles

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,5 +1,29 @@
 export function photoMarkup(data) {
-  data.map(img => {
-    console.log(img);
-  });
+  const gallery = document.querySelector('.gallery');
+  gallery.innerHTML = data
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+            <li class="gallery-item">
+                <a href="${largeImageURL}" class="gallery-link">
+                    <img src="${webformatURL}" alt="${tags}" class="gallery-image" />
+                </a>
+                <div class="info">
+                    <p class="info-item"><span>Likes</span> ${likes}</p>
+                    <p class="info-item"><span>Views</span> ${views}</p>
+                    <p class="info-item"><span>Comments</span> ${comments}</p>
+                    <p class="info-item"><span>Downloads</span> ${downloads}</p>
+                </div>
+            </li>
+        `
+    )
+    .join('');
+  console.log();
 }
